7667f125bf1bfb64a008ddfe250fd6c3c8d14f62
Ahmad Kheil <ahmadkheil313@gmail.com>
2021-09-19 20:29:36 +0300

    Coverting the UI of about component from React Bootstrap to Ant Design

--- components/about.js
+++ components/about.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Container} from 'react-bootstrap'
+import {Layout} from 'antd'
 import { LoremIpsum } from 'react-lorem-ipsum';
 function About() {
     return (
         <>
-            <Container fluid className='about' id='about'>
+            <Layout fluid className='about' id='about'>
                 <center><br /><h1 className='h1title'>About Users List Web Site</h1></center>
                 <center><LoremIpsum p={2} /></center><br />
                 <center><h6>Copyright © 1997 - 2021 · All Rights Reserved</h6></center><br/>
-            </Container>
+            </Layout>
         </>
     )
 }
