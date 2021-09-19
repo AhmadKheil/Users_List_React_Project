0a8f9b224327f8d10c999013fb62e3d847508797
Ahmad Kheil <ahmadkheil313@gmail.com>
2021-09-19 21:41:38 +0300

    Styling about component

--- components/about.js
+++ components/about.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Layout} from 'antd'
 import { LoremIpsum } from 'react-lorem-ipsum';
+import { Content } from 'antd/lib/layout/layout';
 function About() {
     return (
         <>
-            <Layout fluid className='about' id='about'>
+            <Layout id='about' style={{backgroundColor : 'white'}}>
+                <Content className='about'>
                 <center><br /><h1 className='h1title'>About Users List Web Site</h1></center>
                 <center><LoremIpsum p={2} /></center><br />
                 <center><h6>Copyright © 1997 - 2021 · All Rights Reserved</h6></center><br/>
+                </Content>
             </Layout>
         </>
     )
