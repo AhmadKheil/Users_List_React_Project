7015b7fbedef5c2dfd9acb874aa03a6beded4011
Ahmad Kheil <ahmadkheil313@gmail.com>
2021-09-21 10:50:21 +0300

    Convert "Search form" from React-Bootstrap to Ant Design

--- components/seacrhform.js
+++ components/seacrhform.js
@@ -1,20 +1,19 @@
 import React , {useState , useContext} from 'react';
-import { Container , Form , Row , Col , Button , Alert} from 'react-bootstrap'
-import * as Icon from 'react-bootstrap-icons';
 import { styleContext } from './context'
+import {Layout , Row , Col , Form , Input ,  Button , Alert} from 'antd'
+import { Content } from 'antd/lib/layout/layout';
+import { SearchOutlined } from '@ant-design/icons'
 
 function Search({users})
 {
     const [styleId,setStyleId] = useContext(styleContext)
     const [showMessage, setShowMessage] = useState(false)
-    const [headingMessage, setHeadingMessage] = useState('')
     const [message, setMessage] = useState('')
-    const [link , setLink] = useState()
     const [id, setId] = useState('')
+    const [alertId , setAlertId] = useState('')
     const [variant, setVariant] = useState('')
-    
-    const handlesubmit = (e) => {
-        e.preventDefault()
+
+    const handlesubmit = () => {
         const ids = users.map(user => {
             return user.id
         })
@@ -23,79 +22,88 @@ function Search({users})
             if(parseInt(id) === ids[i])
             {
                 setShowMessage(true)
-                setHeadingMessage('Correct Id!')
                 setMessage('This id is correct , the search operation was finished successfully!!')
-                setLink(<Alert.Link href={'#'+id}> Go to this user</Alert.Link>)
                 setVariant('success')
                 setStyleId(id)
+                setAlertId(id)
                 console.log(styleId , id)
                 setId('')
                 return
             }
         }
-        
         setShowMessage(true)
-        setHeadingMessage('Incorrect Id!')
         setMessage('This id is incorrect , search operation failed, please try another Id !')
-        setLink(null)
-        setVariant('danger')
-        setStyleId(id)
+        setVariant('error')
+        setStyleId('')
+        setAlertId(undefined)
+        console.log("ahmad")
         setId('')
         return
     }
+
+
     if (showMessage)
     {
         return (
-        <>
-            <Container className='search align-items-center'>
-                <Row>
-                    <Col sm={7}> <h3><u>If you want to seacrh a specific user , enter his id here!</u></h3></Col>
-                    <Col sm={5}>
-                        <Form className='searchform' onSubmit = {handlesubmit}>
-                        <Row className="align-items-center">
-                            <Col className="my-1">
-                                <Form.Control id="inlineFormInputName" placeholder="Enter ID" type='number' value={id} onChange={(e) => setId(e.target.value)}/>
-                            </Col>
-                            <Col xs="auto" className="my-1">
-                                <Button type="submit"><Icon.Search />{' '} Search</Button>
+            <>
+                <Layout style={{backgroundColor : 'white', border : 3 , borderColor: 'gray' , borderRadius : '50%' , margin : '0 7% 0 7%'}}>
+                    <Content className='search align-items-center'>
+                        <Row style={{margin : '0 4% 0 4%'}}>
+                            <Col sm={15}> <h3><u>If you want to seacrh a specific user , enter his id here!</u></h3></Col>
+                            <Col sm={9}>
+                                <Form className='searchform' onFinish = {handlesubmit}>
+                                    <Row className="align-items-center">
+                                        <Col className='customcol'>
+                                            <Form.Item><Input style={{width : '90%'}} placeholder="Enter ID" type='number' value={id} onChange={(e) => setId(e.target.value)} /></Form.Item>
+                                        </Col>
+                                        <Col>
+                                            <Form.Item> <Button htmlType='submit' style={{borderRadius : '7px'}} type="primary" icon={<SearchOutlined />}>Search</Button></Form.Item>
+                                        </Col> 
+                                     </Row>
+                                </Form>
                             </Col>
                         </Row>
-                        </Form>
-                    </Col>
-                </Row>
-                <br/>
-                <Alert variant={variant} onClose={() => setShowMessage(false)} dismissible>
-                    <Alert.Heading>{headingMessage}</Alert.Heading>
-                    <p>
-                        {message + ' '}
-                        {link}
-                    </p>
-                </Alert>
-            </Container>
+                        <br />
+                        <Alert
+                            style={{ margin: '0 4% 0 4%', height: '100px' }}
+                            type={variant === 'success' ? 'success' : 'error'}
+                            message={message}
+                            action={
+                                alertId ? <a href={'#' + styleId}>Go to this user</a> : ''
+                            }
+                            showIcon
+                            closable
+                            afterClose={() => setShowMessage(false)}
+                        />
+                        <br/>
+                    </Content>
+                </Layout>
         </>
         )
     }
     else {
         return (
-        <>    
-            <Container className='search align-items-center'>
-                <Row>
-                    <Col sm={7}> <h3><u>If you want to seacrh a specific user , enter his id here!</u></h3></Col>
-                    <Col sm={5}>
-                        <Form className='searchform' onSubmit = {handlesubmit}>
-                        <Row className="align-items-center">
-                            <Col className="my-1">
-                                <Form.Control id="inlineFormInputName" placeholder="Enter ID" type='number' value={id} onChange={(e) => setId(e.target.value)}/>
-                            </Col>
-                            <Col xs="auto" className="my-1">
-                            <Button type="submit"><Icon.Search />{' '} Search</Button>
+            <>
+                <Layout style={{backgroundColor : 'white', border : 3 , borderColor: 'gray' , borderRadius : '50%' , margin : '0 7% 0 7%'}}>
+                    <Content className='search align-items-center'>
+                        <Row style={{margin : '2% 4% 0 4%'}}>
+                            <Col sm={15}> <h3><u>If you want to seacrh a specific user , enter his id here!</u></h3></Col>
+                            <Col sm={9}>
+                                <Form className='searchform' onFinish = {handlesubmit}>
+                                    <Row className="align-items-center">
+                                        <Col className='customcol'>
+                                            <Form.Item><Input style={{width : '90%'}} placeholder="Enter ID" type='number' value={id} onChange={(e) => setId(e.target.value)} /></Form.Item>
+                                        </Col>
+                                        <Col>
+                                            <Form.Item> <Button htmlType="submit" style={{borderRadius : '7px'}} type="primary" icon={<SearchOutlined />}>Search</Button></Form.Item>
+                                        </Col> 
+                                     </Row>
+                                </Form>
                             </Col>
                         </Row>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+                    </Content>
+                </Layout>
+            </>
         )
     }
 }
