b868b9d878657cfdb3bfdb4170d6643b439b2e85
Ahmad Kheil <ahmadkheil313@gmail.com>
2021-09-21 10:52:01 +0300

    Convert "All users" component from react bootstrap to Ant Design

--- components/all_users.js
+++ components/all_users.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import {Container , Row} from 'react-bootstrap'
+import {Layout , Row} from 'antd'
 import Single from './single_user'
 
 function Users({users})
 {
+    const {Content} = Layout
     return (
         <>
-            <Container className='cards' id='cards'>
-                <Row>
+            <Layout id='cards' style={{marginTop: '4%' , marginLeft : '7%' , backgroundColor : 'white'}}>
+                <Content>
+                    <Row>
                     {
                         users.map(user => {
                             return <Single key={user.id} data={user}/>
                         })
                     }
                 </Row>
-            </Container>
+                </Content>
+            </Layout>
         </>
     )
 }
